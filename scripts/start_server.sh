85ef3483ffa5bcc43660f377bf3373a291ead265
Grace Farren <gfarren3@ford.com>
2021-04-07 12:02:33 +0100

    updated start_server, install node

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,9 @@
 #objective: instructs application to start on VM 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+npm install
+npm start &> /dev/null &
 
-npm start
+
+
