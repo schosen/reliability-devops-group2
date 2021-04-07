c3f8bdb73f391870b6fb4736d2038c949e4289bb
Grace Farren <gfarren3@ford.com>
2021-04-07 11:37:22 +0100

    cd in folder

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,3 @@
 #objective: instructs application to start on VM 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-npm install
-# node yourServerFile.js &> /dev/null &
+cd reliability-devops-groups2
 npm start
