b87e88f1935327b2eb1cdf14685e12b602fe176f
Grace Farren <gfarren3@ford.com>
2021-04-07 11:32:54 +0100

    start_server added install npm

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,7 @@
 #objective: instructs application to start on VM 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+npm install
+# node yourServerFile.js &> /dev/null &
 npm start
