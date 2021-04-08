6ac2a77238ae5f2d4ff3924d5815b6955a928c49
Grace Farren <gfarren3@ford.com>
2021-04-08 17:21:52 +0100

    updated index.js.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 80;
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.redirect("http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com");
 });
 app.listen(port, () => {
   console.log(`Relay listening at http://localhost:${port}`);
