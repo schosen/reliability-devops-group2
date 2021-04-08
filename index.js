158ae174c428271a608f1ca9aa98f9d1aa91f00c
Grace Farren <gfarren3@ford.com>
2021-04-08 17:33:20 +0100

    created proxy

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
 const port = 80;
-app.get("/", (req, res) => {
-  res.redirect("http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com");
+
+var proxy = require("http-proxy").createProxyServer({
+  host: "http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com",
+  // port: 80
 });
+app.use("/", function (req, res, next) {
+  proxy.web(
+    req,
+    res,
+    {
+      target: "http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com",
+    },
+    next
+  );
+});
+
+// app.get("/", (req, res) => {
+// res.redirect("http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com");
+//   });
+
 app.listen(port, () => {
   console.log(`Relay listening at http://localhost:${port}`);
 });
