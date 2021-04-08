793f74ff214172e91f111f20647b76d73cfc9866
Grace Farren <gfarren3@ford.com>
2021-04-08 16:29:15 +0100

    change start server script to npm start

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,7 +5,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 npm install
-node index.js &> /dev/null &
+npm start &> /dev/null &
 
 #npm install pm2 -g
 #pm2 startOrReload ecosystem.config.json
