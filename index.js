fa34ed79e3babda46e2ef075bee7d5657f50b2e7
Sarah <sarahchosen@hotmail.co.uk>
2021-04-08 19:23:28 +0100

    cforward request to different endpoint

--- index.js
+++ index.js
@@ -2,20 +2,33 @@ const express = require("express");
 const app = express();
 const port = 80;
 
-var proxy = require("http-proxy").createProxyServer({
-  host: "http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com",
-  // port: 80
+
+app.get("/", function (req, res) {
+  req.get({
+    url: "http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com",
+    headers: req.headers,
+  });
+
+  processRequest(req);
+  res.setHeader("Content-Type", "application/json");
+  res.send("Req OK");
 });
-app.use("/", function (req, res, next) {
-  proxy.web(
-    req,
-    res,
-    {
-      target: "http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com",
-    },
-    next
-  );
-});
+
+// var proxy = require("http-proxy").createProxyServer({
+//   host: "http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com",
+//   // port: 80
+// });
+// app.use("/", function (req, res, next) {
+//   proxy.web(
+//     req,
+//     res,
+//     {
+//       target: "http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com",
+//     },
+//     next
+//   );
+// });
+
 
 // app.get("/", (req, res) => {
 // res.redirect("http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com");
