73598af76f87d1f88ea1821c2f71e93e9150e65e
Grace Farren <gfarren3@ford.com>
2021-04-08 17:01:42 +0100

    redirect ec2 to hosp

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express!" });
+  res.redirect("http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com");
 });
 
 module.exports = router;
