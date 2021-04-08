1f3af161bc19bf14c7301a6ac550f030d2f383a8
Grace Farren <gfarren3@ford.com>
2021-04-08 16:20:58 +0100

    changed start script and local for ec2

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,10 +1,12 @@
 #objective: instructs application to start on VM 
 #cd reliability-devops-group2
+cd ../../var/www/html/
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 npm install
 node index.js &> /dev/null &
 
-
+#npm install pm2 -g
+#pm2 startOrReload ecosystem.config.json
 
