9dd47a06509655010f4f492360b91d0247d970f3
Sarah <sarahchosen@hotmail.co.uk>
2021-04-09 11:54:11 +0100

    forward request to different endpoint

--- index.js
+++ index.js
@@ -2,21 +2,19 @@ const express = require("express");
 const app = express();
 const port = 80;
 
-var proxy = require("http-proxy").createProxyServer({
-  host: "http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com",
-  // port: 80
-});
-app.use("/", function (req, res, next) {
-  proxy.web(
-    req,
-    res,
-    {
-      target: "http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com",
-    },
-    next
-  );
+//endpoint for tracking
+app.get('/', function(req, res) {
+  console.log(req)
+  req.get({url: 'http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com', headers: req.headers});
+  //sendRequestToOtherEndPoint(req);
+  processRequest(req);
+  res.setHeader('Content-Type', 'application/json');
+  res.send('Req OK');
 });
 
+function sendRequestToOtherEndPoint(req) {
+  //magic here :)
+}
 
 // app.get("/", (req, res) => {
 // res.redirect("http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com");
