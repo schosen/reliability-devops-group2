6a3818ce0ecc6515cb98b58bfadfc163f9fb9302
Sarah <sarahchosen@hotmail.co.uk>
2021-04-09 16:26:12 +0100

    added retry request for get method

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 const express = require("express");
 const app = express();
 const port = 80;
+const fetch = require("node-fetch");
+const TARGET_SERVER = 'ec2-35-177-40-202.eu-west-2.compute.amazonaws.com'
 
-//endpoint for tracking
-
-app.get("/", function (req, res) {
-  console.log(req);
-  req.get({
-    url: `http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com${req}`,
-    headers: req.headers,
-  });
-  //sendRequestToOtherEndPoint(req);
-  processRequest(req);
-  res.setHeader("Content-Type", "application/json");
-  res.send("Req OK");
+app.get("/*", async (req, res) => {
+  let request = req.originalUrl;
+  console.log(`:: GET ${request}`);
+  let attemptsLeft = 2;
+  let upstreamResponse;
+  while (attemptsLeft > 0) {
+    let upstream = `http://${TARGET_SERVER}${request}`;
+    console.log(`:: Attempt ${2 - attemptsLeft}: ${upstream}`);
+    attemptsLeft = attemptsLeft - 1;
+    upstreamResponse = await fetch(upstream, {
+      headers: { Authorization: req.header("Authorization") },
+    });
+    if (upstreamResponse.ok) {
+      let text = await upstreamResponse.text();
+      res
+        .header("Content-Type", upstreamResponse.headers.get("content-type"))
+        .status(upstreamResponse.status)
+        .send(text);
+      console.log(":: Successful!");
+      return;
+    }
+  }
+  console.log(`:: Failed GET ${request}`);
+  res.status(upstreamResponse.status).send(await upstreamResponse.text());
 });
 
-// function sendRequestToOtherEndPoint(req) {
-//   //magic here :)
-// }
 
-// app.get("/", (req, res) => {
-// res.redirect("http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com");
-//   });
-
-// app.get("/:url", function (req, res) {
-//   url = req.params.url;
-//   console.log(url);
-//   res.send("url stored");
-// });
 
 app.listen(port, () => {
   console.log(`Relay listening at http://localhost:${port}`);
