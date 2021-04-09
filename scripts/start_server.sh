1c7c6359ddd8044b91a4e7a13efb976ca565abba
Sarah <sarahchosen@hotmail.co.uk>
2021-04-09 17:21:53 +0100

    updated start server script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,9 +4,9 @@ cd ../../var/www/html/
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
-npm install pm2 -g
-# npm start &> /dev/null &
-
 npm install
-pm2 startOrReload ecosystem.config.js
+npm start &> /dev/null &
+
+#npm install pm2 -g
+#pm2 startOrReload ecosystem.config.json
 
