fc9c5a13c92f1524c3561a6e2ab99bd9347363e4
Grace Farren <gfarren3@ford.com>
2021-04-09 13:13:32 +0100

    index.js update, /:url

--- index.js
+++ index.js
@@ -3,22 +3,28 @@ const app = express();
 const port = 80;
 
 //endpoint for tracking
-app.get('/', function(req, res) {
-  console.log(req)
-  req.get({url: `http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com/${req}`, headers: req.headers});
-  //sendRequestToOtherEndPoint(req);
-  processRequest(req);
-  res.setHeader('Content-Type', 'application/json');
-  res.send('Req OK');
-});
+// app.get('/', function(req, res) {
+//   console.log(req)
+//   req.get({url: `http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com/${req}`, headers: req.headers});
+//   //sendRequestToOtherEndPoint(req);
+//   processRequest(req);
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send('Req OK');
+// });
 
-function sendRequestToOtherEndPoint(req) {
-  //magic here :)
-}
+// function sendRequestToOtherEndPoint(req) {
+//   //magic here :)
+// }
 
 // app.get("/", (req, res) => {
 // res.redirect("http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com");
 //   });
+
+app.get("/:url", function (req, res) {
+  url = req.params.url;
+  console.log(url);
+  res.send("url stored");
+});
 
 app.listen(port, () => {
   console.log(`Relay listening at http://localhost:${port}`);
