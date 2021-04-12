dddcbc449fed936b12bee7f9641f614ba212fe59
Grace Farren <gfarren3@ford.com>
2021-04-12 10:24:44 +0100

    modified start_server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,9 +4,10 @@ cd ../../var/www/html/
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
-npm install
-npm start &> /dev/null &
+#npm install
+#npm start &> /dev/null &
 
-#npm install pm2 -g
-#pm2 startOrReload ecosystem.config.json
+npm install pm2 
+npm install
+pm2 startOrReload ecosystem.config.json
 
