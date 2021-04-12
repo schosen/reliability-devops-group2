905307e4f2ae3810e9cf55f3e7890364f635f8c9
Grace Farren <gfarren3@ford.com>
2021-04-12 11:48:25 +0100

    deleted unused files

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-// var express = require("express");
-// var router = express.Router();
-
-// /* GET home page. */
-// // router.get("/", function (req, res, next) {
-// //   res.redirect("http://ec2-35-177-40-202.eu-west-2.compute.amazonaws.com");
-// // });
-
-// module.exports = router;
