7e0f582238be250e56bb75ef1ab946d4a930d831
Grace Farren <gfarren3@ford.com>
2021-04-12 11:12:17 +0100

    add rm node_modules to start server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,9 @@
 #objective: instructs application to start on VM 
 #cd reliability-devops-group2
 cd ../../var/www/html/
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
+rm -r node_modules -y
 nvm install node
 npm install
 npm start &> /dev/null &
